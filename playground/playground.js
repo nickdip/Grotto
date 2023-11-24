3b9944ae414ae40a8ce34009ec2038d2452d2cdb
Nick Diplos <nickd321@gmail.com>
2023-11-24 19:40:12 +0000

    started project

---
+++ playground/playground.js
@@ -0,0 +1,41 @@
+const natural = require('natural')
+
+
+
+function analyseText(str) {
+    const matches = []
+    const words = []
+    const tokenizer = new natural.WordTokenizer()
+    const tokenized = tokenizer.tokenize(str)
+    console.log(tokenized) // splits the string into an array of words
+
+    const spellcheck = new natural.Spellcheck(Object.values(toyCategories).flat())
+
+
+
+    tokenized.forEach(wordInText => {
+        spellcheck.getCorrections(wordInText, 1).forEach(correction => {
+            console.log(wordInText, "wordInText", correction, "CORRECTION")
+            words.push(correction)
+        })
+
+    })
+
+    console.log(words, "WORDS")
+
+    console.log(matches, "MATCHES")
+}
+
+
+
+
+console.log(natural.HammingDistance("cad", "car", false))
+
+analyseText("I want a cad for christmas!")
+
+console.log(natural.HammingDistance("karolin", "kathrin", false))
+console.log(natural.HammingDistance("karolin", "kerstin", false))
+
+
+//Jaro–Winkler distance does not give desired result
+
---
+++ playground/toys.js
@@ -0,0 +1,60 @@
+const toyData = require('./toyData.js');
+const natural = require('natural')
+
+class childLetter {
+    constructor(str) {
+        this.tokenizer = new natural.WordTokenizer().tokenize(str)
+        this.toyData = toyData,
+        this.favCategories = {},
+        Object.keys(this.toyData).forEach(category => {
+            this.favCategories[category] = 0
+        }),
+        this.possibleToys = []
+    }
+
+    getToys() {
+        const matches = []
+        this.tokenizer.forEach(word => {
+
+
+            Object.keys(this.toyData).forEach(category => {
+
+                if (word === category) {
+                    this.favCategories[category] += 1
+                    matches.push(word)
+                } 
+                else if (this.toyData[category].includes(word)) {
+                    this.favCategories[category] += 1
+                    matches.push(word)
+                }
+            })
+        })
+
+        this.possibleToys.push(matches)
+    }
+
+    getTypoToys() {
+        const secondaryMatches = []
+        const spellcheck = new natural.Spellcheck(Object.values(this.toyData).flat())
+        this.tokenizer.forEach(wordInText => {
+            spellcheck.getCorrections(wordInText, 1).forEach(correction => {
+                if (correction !== wordInText && !secondaryMatches.includes(correction)) secondaryMatches.push(correction)
+            })
+        })
+        this.possibleToys.push(secondaryMatches)
+
+    }
+
+    showToys() {
+        console.log(this.possibleToys)
+    }
+ 
+    
+}
+
+
+const child1 = new childLetter("I want a doll and a car for christmas!")
+
+child1.getToys()
+child1.getTypoToys()
+child1.showToys()
