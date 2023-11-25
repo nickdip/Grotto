522a77ec1adb781ad531647643be8bc7694be889
imevanc <charal.vag@gmail.com>
2023-11-25 17:08:38 +0000

    add the countdown component

--- src/app/xmasclock/page.js
+++ src/app/xmasclock/page.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import { data } from "@/data";
+import { Countdown } from "@/components";
 
 export default function XmasClock() {
   return (
-    <div className="relative isolate px-6 pt-2 lg:px-8">
+    <div className="relative isolate px-6 pt-2 ">
       <div
         className="absolute inset-x-0 transform-gpu overflow-hidden blur-3xl"
         aria-hidden="true"
@@ -14,23 +16,21 @@ export default function XmasClock() {
           }}
         />
       </div>
-      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-        <div className="text-center">
-          <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-            There are only
-          </h1>
-          <p className="mt-6 text-lg leading-8 text-gray-600">Countdown</p>
-          <h1 className="mt-6 text-2xl font-bold tracking-tight text-green-700 sm:text-5xl">
-            until Christmas!
-          </h1>
-          <div className="mt-10">
-            <Link
-              href="#"
-              className="rounded-md bg-green-700 px-4 py-3 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
-            >
-              Grotto now
-            </Link>
-          </div>
+      <div className="mx-auto py-24 space-y-20">
+        <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-5xl text-center">
+          {data.XmasClock.heading.first}
+        </h1>
+        <Countdown />
+        <h1 className="mt-6 text-2xl font-bold tracking-tight text-green-700 sm:text-5xl text-center">
+          {data.XmasClock.heading.second}
+        </h1>
+        <div className="mt-10 text-center">
+          <Link
+            href="#"
+            className="rounded-md bg-green-700 px-8 py-3 text-baseline font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+          >
+            {data.XmasClock.button}
+          </Link>
         </div>
       </div>
     </div>
--- src/data/index.js
+++ src/data/index.js
@@ -23,4 +23,11 @@ export const data = {
     intro:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
   },
+  XmasClock: {
+    heading: {
+      first: "There are only",
+      second: "until Christmas!",
+    },
+    button: "Grotto now",
+  },
 };
