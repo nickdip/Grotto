5f2f5aa3f19278f797bd13874f3cc77335380136
Nicola Hearn <nfhearn@gmail.com>
2023-11-25 10:39:04 +0000

    adds practice DB page and imports supabase file

--- src/app/config/supabaseClient.js
+++ src/app/config/supabaseClient.js
@@ -2,3 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
---
+++ src/pages/DBPracticeHome.js
@@ -0,0 +1,11 @@
+import supabase from "@/app/config/supabaseClient";
+
+const Home = () => {
+  return (
+    <div className="page home">
+      <h2>Home</h2>
+    </div>
+  );
+};
+
+export default Home;
