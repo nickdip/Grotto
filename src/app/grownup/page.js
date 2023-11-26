f596e1a589d4e119941004b3e50ac989b3939014
Nicola Hearn <nfhearn@gmail.com>
2023-11-26 13:27:53 +0000

    adds styling to finished letter

--- src/app/grownup/page.js
+++ src/app/grownup/page.js
@@ -1,15 +1,18 @@
 //display child's letter (get the data from database)
 //display the child's suggested gifts (get the data from database)
 
+import "./style.css";
+//style
 //components
-import { FinishedLetter } from "@/components/FinishedLetter";
-
+import { FinishedLetter } from "../../components/FinishedLetter";
 const GrownUp = () => {
   return (
-    <div>
-      <h1>Hello user</h1>
-      <p>some text here</p>
-      <FinishedLetter />
+    <div className="parent-page">
+      <div>
+        <h1>Hello user</h1>
+        <FinishedLetter />
+      </div>
+      <div>{/* //suggested toys */}</div>
     </div>
   );
 };
--- src/components/FinishedLetter.jsx
+++ src/components/FinishedLetter.jsx
@@ -1,6 +1,8 @@
+import "../app/grownup/style.css";
+
 export const FinishedLetter = () => {
   return (
-    <div>
+    <div className="finished-letter">
       <p>Dummy Text</p>
     </div>
   );
