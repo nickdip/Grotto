ffd1865e4d45454fc743f9c5ef63712782f1ac5b
Nicola Hearn <nfhearn@gmail.com>
2023-11-26 13:43:59 +0000

    fetches username from the database

--- src/app/grownup/page.js
+++ src/app/grownup/page.js
@@ -1,20 +1,43 @@
-//display child's letter (get the data from database)
-//display the child's suggested gifts (get the data from database)
-
+"use client";
+import supabase from "@/config/supabaseClient";
+import { useEffect, useState } from "react";
 import "./style.css";
 //style
 //components
 import { FinishedLetter } from "../../components/FinishedLetter";
 import { SuggestedToys } from "@/components/SuggestedToys";
+import { ToyCard } from "@/components/ToyCard";
 const GrownUp = () => {
+  const [fetchError, setFetchError] = useState(null);
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  async function fetchUser() {
+    const { data, error } = await supabase.from("grown-ups").select();
+
+    if (error) {
+      setFetchError("COuld not fetch the user");
+      setUser(null);
+      console.log(error);
+    }
+    if (data) {
+      setUser(data);
+      console.log(data);
+    }
+  }
+
   return (
     <div className="parent-page">
       <div>
-        <h1>Hello user</h1>
+        <h1>Hello {user[0].first_name}</h1>
         <FinishedLetter />
       </div>
       <div>
-        <SuggestedToys />
+        <h1>SUGGESTED TOYS</h1>
+        {/* <ToyCard /> */}
       </div>
     </div>
   );
