d4ed13b1eaee3ef1e7aed031a128e2e857a4f9dd
Nicola Hearn <nfhearn@gmail.com>
2023-11-26 10:54:02 +0000

    styling updated

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -6,7 +6,7 @@ export const ToyCard = ({ toy }) => {
   return (
     <div className="toy-card">
       <h2>{toy.name}</h2>
-      <img src={toy.image_link} style={{ height: "40%" }}></img>
+      <img src={toy.image_link}></img>
       <h2>£{toy.price.toFixed(2)}</h2>
       <p>{toy.description}</p>
       <p>Sustainable choice: {toy.sustainability}</p>
