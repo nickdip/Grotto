8a77d70220670c199820d1c2a4d6c9dd473a02f8
imevanc <charal.vag@gmail.com>
2023-11-26 00:17:57 +0000

    update the input onchange

--- src/components/SignInButton.jsx
+++ src/components/SignInButton.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export const SignInButton = () => (
-  <Link href="">
+  <Link href="/sign-in">
     <p className="shadow-xl inline-flex items-center rounded-full border border-transparent bg-green-700 px-3 sm:px-6 py-2 text-lg sm:text-xl font-lg text-white font-bold hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2">
       Sign In
     </p>
